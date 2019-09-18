3b017133ce87dd031a5a3e1fbf61bf27c24eeb4a
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2019-09-18 17:23:00 -0400

    Remove Central Africa

--- R/data.R
+++ R/data.R
@@ -3,22 +3,6 @@
 # use devtools::load_all() to load during package development
 # enables ?data("datasetname")
 
-#' Plasmodium falciparum parasite rate in 2-10 year olds in 2000-2017 in Central Africa
-#'
-#' A dataset containing longitude, latitude, point estimates, and credible intervals bounds
-#'
-#' @format A data frame with 144000 rows and 5 variables:
-#' \describe{
-#'   \item{x}{Longitude in decimal degrees}
-#'   \item{y}{Latitude in decimal degrees}
-#'   \item{Median}{The median posterior predictive rate of Plasmodium falciparum in 2-10 year olds, 2000-2017}
-#'   \item{UCI}{The lower extreme of the credible interval around the median posterior predictive rate of Plasmodium falciparum in 2-10 year olds, 2000-2017}
-#'   \item{UCI}{The upper extreme of the credible interval around the median posterior predictive rate of Plasmodium falciparum in 2-10 year olds, 2000-2017}
-#' }
-#' @source \url{https://map.ox.ac.uk/}
-"CentralAfrica_Pf_incidence"
-
-
 #' Plasmodium falciparum parasite rate in 2-10 year olds in 2000-2017 in Sub-Saharan Africa
 #'
 #' A dataset containing longitude, latitude, point estimates, and credible intervals bounds
@@ -35,20 +19,6 @@
 "SubSaharanAfrica_Pf_incidence"
 
 
-#' Shape files for Central Africa
-#'
-#' A SpatialPolygonsDataFrame for Central Africa
-#'
-#' @format A SpatialPolygonsDataFrame with dimensions 14 and 16
-#' \describe{
-#'   \item{iso}{To-be-completed}
-#'   \item{admn_level}{To-be-completed}
-#'   ...
-#' }
-#' @source Obtained using malariaAtlas::getShp (see data-raw/get_shape_files.R)
-"CentralAfrica_shp"
-
-
 #' Shape files for Sub-Saharan Africa
 #'
 #' A SpatialPolygonsDataFrame for Sub-Saharan Africa
@@ -57,7 +27,6 @@
 #' \describe{
 #'   \item{iso}{To-be-completed}
 #'   \item{admn_level}{To-be-completed}
-#'   ...
 #' }
 #' @source Obtained using malariaAtlas::getShp (see data-raw/get_shape_files.R)
 "SubSaharanAfrica_shp"
--- data-raw/format_pf_incidence.R
+++ data-raw/format_pf_incidence.R
@@ -1,7 +1,6 @@
 library(raster) # Suggests
 library(dplyr) # Imports
 # library(rgdal) # James, is this really needed?
-# library(malariaAtlas) # James, is this really needed?
 rm(list=ls())
 
 # Import point estimates and lower and upper credible interval bounds
@@ -18,46 +17,6 @@ pf_incid_UCI <- as.data.frame(pf_incid_UCI, xy = TRUE)
 colnames(pf_incid_median)[3] = 'z'
 colnames(pf_incid_LCI)[3] = 'z'
 colnames(pf_incid_UCI)[3] = 'z'
-
-
-# Central Africa ---------------------------------------
-
-# The lat-log intervals of interest
-lat_long_intervals = c(15,35,-7.5,5)
-
-# Filter by lat long intervals
-pf_incid_median = dplyr::filter(pf_incid_median,
-                              x > lat_long_intervals[1],
-                              x < lat_long_intervals[2],
-                              y > lat_long_intervals[3],
-                              y < lat_long_intervals[4])
-pf_incid_LCI = dplyr::filter(pf_incid_LCI,
-                                 x> lat_long_intervals[1],
-                                 x< lat_long_intervals[2],
-                                 y> lat_long_intervals[3],
-                                 y< lat_long_intervals[4])
-pf_incid_UCI = dplyr::filter(pf_incid_UCI,
-                      x> lat_long_intervals[1],
-                      x< lat_long_intervals[2],
-                      y> lat_long_intervals[3],
-                      y< lat_long_intervals[4])
-
-# Check they all match
-writeLines('check all are zeros..')
-sum((pf_incid_median$x != pf_incid_LCI$x) & (pf_incid_median$x != pf_incid_UCI$x))
-sum((pf_incid_median$y != pf_incid_LCI$y) & (pf_incid_median$y != pf_incid_UCI$y))
-
-# Create and save data frame
-CentralAfrica_Pf_incidence = data.frame(x=pf_incid_median$x,
-                        y = pf_incid_median$y,
-                        Median = pf_incid_median$z,
-                        UCI = pf_incid_UCI$z,
-                        LCI = pf_incid_LCI$z)
-
-# Save example data
-usethis::use_data(CentralAfrica_Pf_incidence, overwrite = TRUE)
-
-# Sub-Saharan Africa ----------------------------------
 
 # The lat-log intervals of interest
 lat_long_intervals = c(-17,52,-26,21)
