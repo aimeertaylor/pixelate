c0615c95ac5fefb1af58ee3f7e6578c476dbb17d
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2019-09-18 16:16:12 -0400

    Add shape in prep for vignettes

---
+++ data-raw/get_shape_files.R
@@ -0,0 +1,6 @@
+library(malariaAtlas)
+
+CentralAfrica_shp <- malariaAtlas::getShp(lat = c(-7.5,5),long = c(15,35),admin_level = "admin0")
+SubSaharanAfrica_shp <- malariaAtlas::getShp(lat = c(-26,21),long = c(-17,52),admin_level = "admin0")
+usethis::use_data(CentralAfrica_shp, SubSaharanAfrica_shp, overwrite = TRUE)
+
