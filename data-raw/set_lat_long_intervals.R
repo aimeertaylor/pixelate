be78e1326297b5c01051f6325316d8b264f02c53
Aimee Taylor <aimee.r.taylor.85@gmail.com>
2019-09-24 16:02:21 -0400

    Convert lat lon to data frame for clearer indexing and fix sp. mistake in saved output

--- data-raw/set_lat_long_intervals.R
+++ data-raw/set_lat_long_intervals.R
@@ -1,3 +1,3 @@
 # Set the lat-log intervals of interest and save
-lat_long_intervals <- c(-17,52,-26,21)
-save(lat_long_intervals, file = "data-raw/lat_log_intervals.RData")
+lat_long_intervals <- data.frame(lon_min = -17, lon_max = 52, lat_min = -26, lat_max = 21)
+save(lat_long_intervals, file = "data-raw/lat_long_intervals.RData")
