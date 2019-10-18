04cf6229cde89c7f716c296ec54ade26a219f07f
artaylor85 <aimee.r.taylor.85@gmail.com>
2019-10-18 11:04:48 -0400

    Update with row names

--- data-raw/set_lat_long_intervals.R
+++ data-raw/set_lat_long_intervals.R
@@ -3,6 +3,8 @@ lat_long_intervals <- data.frame(lon_min = c(-18,11.7),
                                  lon_max = c(52, 35.5),
                                  lat_min = c(-26.5,-14),
                                  lat_max = c(18, 5.7))
+
+rownames(lat_long_intervals) = c("SubSaharanAfrica", "CentralAfrica")
 save(lat_long_intervals, file = "data-raw/lat_long_intervals.RData")
 
 
